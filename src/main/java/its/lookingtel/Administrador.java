f0883a936d6b292c407b9cbf7ad449aec3767e9b
MobiousKnight98 <enriquehernandez.pereyra@gmail.com>
2023-03-02 12:30:59 -0600

    Cambios nuevos

---
+++ src/main/java/its/lookingtel/Administrador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package its.LookingTel;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Guest Mode
+ */
+
+public class Administrador extends Querys  {
+
+   
+   void Iniciar_Sesion(String correo_electronico,String contraseña){
+       
+   }
+    
+}
